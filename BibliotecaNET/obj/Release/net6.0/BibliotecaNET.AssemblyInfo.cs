e328eff631fc7c39a96324c75f555d63051bfdaa
Johann Casallas <casallasbecerrajohann@gmail.com>
2024-04-18 17:15:18 -0500

    Se ajusta Api Login

--- BibliotecaNET/obj/Release/net6.0/BibliotecaNET.AssemblyInfo.cs
+++ BibliotecaNET/obj/Release/net6.0/BibliotecaNET.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaNET")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596c688ea1132ac19f577befc892f7e5af7a41b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaNET")]
